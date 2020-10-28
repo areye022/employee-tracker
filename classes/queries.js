9d0aec501d6b29077fd327b6c097317948799b2e
areye022 <areye022@gmail.com>
2020-10-27 21:16:07 -0700

    put connections into their own js file, and created an updateTracker file that can now prompt the departments table, and can also add new departments. Unfortunately was having issues when trying to incorporate the classes into the server.js file

---
+++ classes/queries.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const util= require("util");
+const connection= require("../connection")
+
+// one class with all queries to represent all tables 
+class Queries {
+    constructor(connection) {
+        // this class represents the connection to the database
+
+        console.log("inside queries");
+        
+        this.connection = connection;
+        
+    }
+    getDepartment() {
+        return this.connection.query("SELECT * FROM department")
+    }
+
+};
+
+
+module.exports= Queries
--- server.js
+++ server.js
@@ -1,85 +1,76 @@
-var mysql= require('mysql');
 const fs = require("fs");
 const inquirer = require("inquirer");
-const util= require("util");
-const cTable = require("console.table")
+const cTable = require("console.table");
 
 // classes
-const Department = require("./classes/department");
-
-// output tables
-var department = [];
-
-// mysql coinfiguration object
-var connection = mysql.createConnection({
-    // host is the location of your local server
-    host: "localhost",
-    port: 3306,
-    user:"root",
-    password:"FlyingMonkeys1!",
-    database: "employees_db",
-});
+const Queries = require("./classes/queries");
+const connection= require("./connection")
 
 // currently only one function is prompting
 // to see all roles
-function viewRoles(){
-    connection.query("SELECT * FROM role", function(err, res){
-        if (err) throw (err);
-// console.logging the results from the songs table
-        console.table(res);
-    })
-}
-
-// to see departments 
-function viewDepartments(){
-    connection.query("SELECT * FROM department"), function (err, res){
-        if (err) throw(err);
-        console.table(res);
-    }
-}
-
-// to see employees table
-function viewEmployees(){
-    connection.query("SELECT * FROM employee"), function (err, res){
-        if (err) throw(err);
-        console.table(res);
-    }
-}
-
-// const updateTracker=() =>{
-//     inquirer.prompt([
-//         {
-//             type: "list",
-//             message: "What would you like to do?",
-//             name: "toDo",
-//             choices:["view departments"]
-//         }
-//     ]).then(res =>{
-//         const { toDo } = res;
-//         switch (toDo) {
-//             case "view departments":
-// OR CALL FUNCTION, BUT NOT WORKING PROPERLY
-//                     connection.query("SELECT * FROM department", function(err,res){
-//                             if (err) throw (err);
-//                     // console.logging the results from the songs table
-//                             console.table(res);
-                            
-//                         })
-//                     }
-                    
-//         }).then(res =>{
-//         getDepartment();
+// function viewRoles(){
+//     connection.query("SELECT * FROM role", function(err, res){
+//         if (err) throw (err);
+// // console.logging the results from the songs table
+//         console.table(res);
 //     })
 // }
 
+// // to see departments 
+async function viewDepartments(){
+    // var departments = await Queries.getDepartment();
+                console.table(connection.query("SELECT * FROM department"));
+    }
 
-connection.connect(function(err){
-    if (err) throw err;
 
-    console.log(`connected as id ${connection.threadId}`);
-    // viewRoles();
-    // viewDepartments();
-    viewEmployees();
-    // updateTracker();
-    connection.end();
+// // to see employees table
+// function viewEmployees(){
+//     connection.query("SELECT * FROM employee"), function (err, res){
+//         if (err) throw(err);
+//         console.table(res);
+//     }
+// }
+
+async function updateTracker(){
+    const { toDo } = await inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "toDo",
+            choices:["view departments","add department"]
+        }
+    ])
+        switch (toDo) {
+            case "view departments":
+// OR CALL FUNCTION, BUT NOT WORKING PROPERLY
+connection.query("SELECT * FROM department", function(err,res){
+    console.table(res);
 })
+            case "add department":
+                inquirer.prompt([
+                    {
+                        type:"input",
+                        message:"what department would you like to add",
+                        name:"departmentName"
+                    }
+                ]).then(res=>{
+                    connection.query("INSERT INTO department (name) VALUES (?)", [res.departmentName], function(err,res2){
+                        console.table(res2);
+                    })
+                })
+
+    }
+}
+
+updateTracker();
+
+// connection.connect(function(err){
+//     if (err) throw err;
+
+//     console.log(`connected as id ${connection.threadId}`);
+//     // viewRoles();
+//     // viewDepartments();
+//     // viewEmployees();
+//     updateTracker();
+//     connection.end();
+// })
